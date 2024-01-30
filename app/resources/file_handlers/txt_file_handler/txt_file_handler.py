6736730125d465946de3482610577a143b31e997
sekvi <filmatkozlik@gmail.com>
2024-01-30 19:32:02 +0100

    Added missing docs strings

--- app/resources/file_handlers/txt_file_handler/txt_file_handler.py
+++ app/resources/file_handlers/txt_file_handler/txt_file_handler.py
@@ -6,11 +6,23 @@ import time
 class TxtFileHandler(AbstractFileHandler):
      
     def read_file(self) -> str:
+        """
+        Function to read from provided file path.
+
+        Returns:
+            str: String representation of file content.
+        """
         with open(self.file_path, 'r') as file:
             content = file.read()
             return content
     
     
     def write_file(self, message: str) -> None:
+        """
+        Function to wrtie to provided file.
+
+        Args:
+            message (str): String representation of message to save.
+        """
         with open(f'Decoded_{time.time()}', 'w') as file:
             file.write(message)
